b7fb4fd08b2931286a60a9316f5613ef990533d1
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2020-11-23 18:49:45 +0100

    I started the loop 1 step 2 challenge

---
+++ Loop 1 - Step 2/script.js
@@ -0,0 +1,14 @@
+let input = document.querySelector('#input');
+let button = document.querySelector('#tab');
+let positive = document.querySelector('#positive');
+let negative = document.querySelector('#negative');
+let neutral = document.querySelector('#neutral');
+let answer = document.querySelector('#answer');
+let active = document.querySelector('.active');
+
+
+
+
+let select = function() {
+
+}
