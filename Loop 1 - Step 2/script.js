b92b115e0486f513c113372ba0eeb079c72cda99
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-01-10 18:07:11 +0100

    the element is finished

--- Loop 1 - Step 2/script.js
+++ Loop 1 - Step 2/script.js
@@ -5,10 +5,43 @@ let negative = document.querySelector('#negative');
 let neutral = document.querySelector('#neutral');
 let answer = document.querySelector('#answer');
 let active = document.querySelector('.active');
+let li = document.querySelectorAll('li');
 
 
+tab = {
+    active: 'Positive content',
+    negative: 'Negative content',
+    neutral: 'Neutral content'
+};
+
+let arr = [positive, negative, neutral];
 
 
-let select = function() {
+positive.addEventListener('click', changArr);
+negative.addEventListener('click', changArr);
+neutral.addEventListener('click', changArr);
+button.addEventListener('click', checkTab);
 
+function changArr() {
+    arr.forEach(x => x.classList.remove('active'));
+    this.classList.add('active');
+    if (this == positive) { console.log('click on pos'); return answer.textContent = tab.active };
+    if (this == negative) { console.log('click on neg'); return answer.textContent = tab.negative };
+    if (this == neutral) { console.log('click on neutral'); return answer.textContent = tab.neutral };
+}
+
+function checkTab() {
+
+    arr.forEach(x => x.classList.remove('active'));
+
+    this.classList.add('active');
+    if (input.value == "1") {
+        return answer.textContent = tab.active;
+    } else if (input.value == "2") {
+        return answer.textContent = tab.negative;
+    } else if (input.value == "3") {
+        return answer.textContent = tab.neutral;
+    } else {
+        return prompt('Index is invalid');
+    }
 }
