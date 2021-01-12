fb96828c953621589bf0951f1618fcee95e39636
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-01-12 06:28:16 +0100

    finished front loop element loop 1 step 3

--- Loop 1 - Step 3/script.js
+++ Loop 1 - Step 3/script.js
@@ -1,11 +1,11 @@
 const toggles = document.querySelectorAll('.toggle-switch');
 
 
-const change = function(toggle) {
+const change = function(i) { //need to put the i argument to be able later to find the calling add even listener
 
-    toggles.forEach(e => {
-        if (e === init) {
-            e.currentTarget.classList.toggle('active');
+    toggles.forEach(e => { //check again over the differents toggles
+        if (e === i.currentTarget) { // if the toggle e is equal to the toggle who call the even listener
+            i.currentTarget.classList.toggle('active');
         } else {
             number = Math.floor(Math.random() * 2);
             if (number == 1) {
@@ -17,5 +17,5 @@ const change = function(toggle) {
 
 };
 toggles.forEach(toggle => {
-    toggle.addEventListener("click", change);
+    toggle.addEventListener("click", change); //never put the () in the add event listener because it's calling the function while refresh the page;
 });
