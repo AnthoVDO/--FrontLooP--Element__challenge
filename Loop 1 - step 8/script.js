9c837407820e758268a055775849fd51e7bd2e30
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-01-20 12:35:47 +0100

    finished element loop 1 step 8

--- Loop 1 - step 8/script.js
+++ Loop 1 - step 8/script.js
@@ -4,18 +4,16 @@ const btnHiddeBack = document.querySelectorAll('.btn-hidde-back');
 const btnClose = document.querySelectorAll('.btn-close');
 const search = document.querySelector('.search');
 const run = document.querySelector('.run');
-const blackBox1 = document.querySelector('.black-box-1');
-const blackBox2 = document.querySelector('.black-box-2');
+const blackBox = document.querySelector('.black-box');
 
 const go = function(e) {
     if (e.currentTarget == btnSearch) {
         search.style.right = "0";
-        blackBox1.style.opacity = "0.7";
     }
     if (e.currentTarget == btnRun) {
         run.style.right = "0";
-        blackBox2.style.opacity = "0.7";
     }
+    blackBox.classList.remove('hidden-box');
 }
 
 btnSearch.addEventListener("click", go);
@@ -25,12 +23,11 @@ btnRun.addEventListener("click", go);
 const goBack = function(e) {
     if (e.currentTarget == btnHiddeBack[0] || btnHiddeBack[1]) {
         search.style.right = "100vw"
-        blackBox1.style.opacity = "0";
     }
     if (e.currentTarget == btnClose[0] || btnClose[1]) {
         run.style.right = "100vw"
-        blackBox2.style.opacity = "0";
     }
+    blackBox.classList.add('hidden-box');
 }
 
 btnHiddeBack.forEach(element => element.addEventListener("click", goBack));
