5f30dd45d2b6191eaf94518f682130aa39f80f0a
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-01-23 17:07:39 +0100

    finished front loop element loop 1 step 10

---
+++ Loop 1 - step 10/script.js
@@ -0,0 +1,16 @@
+const card = document.querySelector('.card');
+const container = document.querySelector(".container");
+cardPosition = card.getBoundingClientRect();
+
+
+container.addEventListener("dragover", (e) => {
+    e.preventDefault();
+})
+
+container.addEventListener('drop', (e) => {
+
+    e.preventDefault();
+
+    card.style.top = `${e.pageY-100}px`;
+    card.style.left = `${e.pageX-100}px`;
+})
