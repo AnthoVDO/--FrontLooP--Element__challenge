b3853783c5294f6e0333ca41adec7836a7faa8e7
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-01-27 22:16:14 +0100

    I continue frontloop element loop 2 - step 3

---
+++ Loop 2 - step 3/script.js
@@ -0,0 +1,5 @@
+const previous = document.querySelector('.carousel--prev');
+const next = document.querySelector('.carousel--next');
+const box = document.querySelector('.carousel__box');
+
+//box.style.transform = "translate(-5%)";
