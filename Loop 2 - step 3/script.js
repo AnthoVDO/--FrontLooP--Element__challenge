37953fbb5e9b0047af6484241e09ffd051ab6a0a
Anthony Van Den Ostende <anthony.vdo@hotmail.be>
2021-01-28 21:51:55 +0100

    I continued the front loop element loop 2 step 3

--- Loop 2 - step 3/script.js
+++ Loop 2 - step 3/script.js
@@ -1,5 +1,3 @@
 const previous = document.querySelector('.carousel--prev');
 const next = document.querySelector('.carousel--next');
 const box = document.querySelector('.carousel__box');
-
-//box.style.transform = "translate(-5%)";
